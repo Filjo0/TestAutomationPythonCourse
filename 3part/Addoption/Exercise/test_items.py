e90bf5e64398cf17fbfb591c2eed5f99d17eaf88
Philipp <55308835+Filjo0@users.noreply.github.com>
2020-09-12 23:09:20 +0800

    Initial commit

--- 3part/Addoption/Exercise/test_items.py
+++ 3part/Addoption/Exercise/test_items.py
@@ -3,7 +3,7 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_es(browser):
+def test_find_card_button(browser):
     browser.get(link)
     time.sleep(30)
     browser.implicitly_wait(10)
