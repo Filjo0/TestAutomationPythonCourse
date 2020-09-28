cd0567c0912bc477c1b010c9d1f267dc46fcd6e0
Philipp <55308835+Filjo0@users.noreply.github.com>
2020-09-28 16:43:49 +0800

    Main Page update

--- 4part/pages/main_page.py
+++ 4part/pages/main_page.py
@@ -1,5 +1,3 @@
-from selenium.webdriver.common.by import By
-
 from .base_page import BasePage
 from .locators import MainPageLocators
 
@@ -8,6 +6,8 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
+        alert = self.browser.switch_to.alert
+        alert.accept()
 
     def should_have_login_link(self):
         assert self.is_element_found(*MainPageLocators.LOGIN_LINK), "Login link not found"
