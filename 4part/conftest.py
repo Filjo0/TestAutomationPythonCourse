7d50f2ccaa83a26c53bb7c2ccf732893690e4ab1
filjo <Filjo@mail.ru>
2023-04-23 17:17:29 +0800

    Refactoring.

--- 4part/conftest.py
+++ 4part/conftest.py
@@ -30,4 +30,4 @@ def browser(request):
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser..")
-    quit()
+    browser.quit()
